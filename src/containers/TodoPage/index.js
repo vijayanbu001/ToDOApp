36cf5d3bef75bd6bde9c2a05ad88055743978a60
Vijay <vijayanbu001@gmail.com>
2018-09-02 19:39:19 +0530

    formating

--- src/containers/TodoPage/index.js
+++ src/containers/TodoPage/index.js
@@ -20,7 +20,4 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(ToDoPage)
+export default connect(mapStateToProps, mapDispatchToProps)(ToDoPage)
